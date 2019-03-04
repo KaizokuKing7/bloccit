3fe6bf1b1ecc5e9d12c48e96f4520a672d367c45
Stanley Pierre-Canel <thousandsunnygo@outlook.com>
2019-03-03 23:54:27 -0500

    Revert "create and read done"
    This reverts commit 176d03e53eedc84141ef385d4c54e20a7d65c6d5.

--- src/routes/advertisements.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const advertController = require("../controllers/advertController");
-
-router.get("/advertisements", advertController.index);
-router.get("/advertisements/new", advertController.new);
-router.post("/advertisements/create", advertController.create);
-
-module.exports = router;
