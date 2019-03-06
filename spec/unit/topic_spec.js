8e4935e431b7c3ad672b8e14243eb50925c65b0d
Stanley Pierre-Canel <thousandsunnygo@outlook.com>
2019-03-06 10:11:16 -0500

    checkpoint authorization

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,80 @@
+const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
+const sequelize = require("../../src/db/models/index").sequelize;
+const User = require("../../src/db/models").User;
+
+
+describe("Topic", () => {
+    beforeEach((done) => {
+        this.topic;
+        this.post;
+        this.user;
+    
+        sequelize.sync({ force: true }).then((res) => {
+    
+            // #2
+            User.create({
+                email: "starman@tesla.com",
+                password: "Trekkie4lyfe"
+            })
+                .then((user) => {
+                    this.user = user; //store the user
+    
+                    // #3
+                    Topic.create({
+                        title: "Expeditions to Alpha Centauri",
+                        description: "A compilation of reports from recent visits to the star system.",
+    
+                        // #4
+                        posts: [{
+                            title: "My first visit to Proxima Centauri b",
+                            body: "I saw some rocks.",
+                            userId: this.user.id
+                        }]
+                    }, {
+    
+                            // #5
+                            include: {
+                                model: Post,
+                                as: "posts"
+                            }
+                        })
+                        .then((topic) => {
+                            this.topic = topic; //store the topic
+                            this.post = topic.posts[0]; //store the post
+                            done();
+                        })
+                })
+        });
+    });
+    describe("#create()", () => {
+        it("Should create a topic", (done) => {
+            Topic.create({
+                title: "News",
+                description: "Weather at eleven"
+            })
+                .then((topic) => {
+                    expect(topic.title).toBe("News")
+                    expect(topic.description).toBe("Weather at eleven")
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+        });
+
+    });
+
+
+    describe("#getPosts()", () => {
+        it("Should return the associated posts", (done) => {
+            this.topic.getPosts()
+                .then((posts) => {
+                    expect(posts[0].title).toBe("My first visit to Proxima Centauri b")
+                    done();
+                })
+
+        });
+    })
+})
